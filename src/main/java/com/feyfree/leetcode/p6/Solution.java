39c7418ae77704ee0cf7ae2b08ae83af4a8e5681
leilei <gengll2@yunjiglobal.com>
2020-06-10 19:12:53 +0800

    [REV] add p6 Solution template

---
+++ src/main/java/com/feyfree/leetcode/p6/Solution.java
@@ -0,0 +1,17 @@
+package com.feyfree.leetcode.p6;
+
+/**
+ * zigzag conversion
+ * Z 字形 变换输出
+ *
+ * @author leilei
+ */
+public class Solution {
+
+    public String convert(String s, int numRows) {
+        if (s == null) {
+            return "";
+        }
+        return null;
+    }
+}
