b51fd7f6e997ae224016a863e03271e6686b7e09
feyfree <feyfree@163.com>
2020-06-23 18:54:21 +0800

    [ADD] add solution tips

--- src/main/java/com/feyfree/leetcode/p7/Solution.java
+++ src/main/java/com/feyfree/leetcode/p7/Solution.java
@@ -3,11 +3,17 @@ package com.feyfree.leetcode.p7;
 /**
  * reverse-integer
  * 整数反转
+ * <p>
+ * java 类
+ * Integer.MAX_VALUE
+ * Integer.MIN_VALUE
  *
  * @author Leilei
  */
 public class Solution {
     public int reverse(int x) {
+
+
         return 0;
     }
 }
