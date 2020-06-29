0a5e1e0da28956f998046fdd6de663b9180c8525
feyfree <feyfree@163.com>
2020-06-29 17:36:55 +0800

    [ADD] add comment

--- src/main/java/com/feyfree/leetcode/p11/Solution.java
+++ src/main/java/com/feyfree/leetcode/p11/Solution.java
@@ -4,6 +4,8 @@ package com.feyfree.leetcode.p11;
  * 盛水最多的容器
  * <p>
  * container-with-most-water
+ * <p>
+ * 区间容量取决于开始和结束的 短板
  *
  * @author leilei
  */
@@ -12,10 +14,14 @@ public class Solution {
         if (height.length <= 1) {
             return 0;
         }
+        // 区间起点
         int start = 0;
+        // 区间结束
         int end = height.length - 1;
+        // 最终容量
         int tempCapacity = 0;
         while (start <= end) {
+            // 如果末尾的高度高于开始的高度  则开始的高度  ++1
             if (height[end] > height[start]) {
                 int cal = (end - start) * height[start];
                 tempCapacity = Math.max(cal, tempCapacity);
