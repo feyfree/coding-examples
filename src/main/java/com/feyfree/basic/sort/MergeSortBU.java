710eeb4c50db04e70c14ffda9bd9470347e5499a
feyfree <feyfree@163.com>
2020-07-09 16:30:33 +0800

    [ADD] add new test

--- src/main/java/com/feyfree/basic/sort/MergeSortBU.java
+++ src/main/java/com/feyfree/basic/sort/MergeSortBU.java
@@ -1,9 +1,26 @@
 package com.feyfree.basic.sort;
 
+import java.util.List;
+
 /**
  * 自下而上归并排序
  *
  * @author leilei
  */
 public class MergeSortBU {
+    public void sort(int[] data) {
+
+    }
+
+    public void merge(int[] data, int[] aux, int lo, int mid, int hi) {
+        if (hi + 1 - lo >= 0) {
+            System.arraycopy(data, lo, aux, lo, hi + 1 - lo);
+        }
+        int i = lo, j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                data[k] = aux[j++];
+            }
+        }
+    }
 }
---
+++ src/test/java/com/feyfree/basic/operation/TestOperation.java
@@ -0,0 +1,11 @@
+package com.feyfree.basic.operation;
+
+import org.junit.Test;
+
+public class TestOperation {
+    @Test
+    public void testPlusPlus() {
+        int j = 0;
+        System.out.println(j ++);
+    }
+}
