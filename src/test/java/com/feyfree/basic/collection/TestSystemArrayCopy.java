49b623547e4159bb4a1647663a6c2cb7a94e31c0
leilei <gengll2@yunjiglobal.com>
2020-07-14 15:05:25 +0800

    [REV] add comment on System.arrayCopy()

--- src/test/java/com/feyfree/basic/collection/TestSystemArrayCopy.java
+++ src/test/java/com/feyfree/basic/collection/TestSystemArrayCopy.java
@@ -11,6 +11,7 @@ public class TestSystemArrayCopy {
         int[] a = new int[]{0, 1, 2, 3};
         int mid = 3;
         int[] pre = new int[mid];
+        // System.arrayCopy() 中的srcPos 从该起点包含该起点的复制的
         System.arraycopy(a, 0, pre, 0, mid);
         int[] after = new int[a.length - mid];
         System.arraycopy(a, mid, after, 0, a.length - mid);
