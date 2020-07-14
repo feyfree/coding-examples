314b0c86985a983c414f19501844fe7e3b6c140b
leilei <gengll2@yunjiglobal.com>
2020-07-14 14:47:21 +0800

    [REV] add p23 solution

---
+++ src/test/java/com/feyfree/basic/collection/TestSystemArrayCopy.java
@@ -0,0 +1,20 @@
+package com.feyfree.basic.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class TestSystemArrayCopy {
+
+    @Test
+    public void testSystemArrayCopy() {
+        int[] a = new int[]{0, 1, 2, 3};
+        int mid = 3;
+        int[] pre = new int[mid];
+        System.arraycopy(a, 0, pre, 0, mid);
+        int[] after = new int[a.length - mid];
+        System.arraycopy(a, mid, after, 0, a.length - mid);
+        System.out.println(Arrays.toString(pre));
+        System.out.println(Arrays.toString(after));
+    }
+}
