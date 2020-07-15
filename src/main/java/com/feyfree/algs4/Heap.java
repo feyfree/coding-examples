d26024e666f1ccac4fcad7079d2c2510e48c1931
leilei <gengll2@yunjiglobal.com>
2020-07-15 19:02:49 +0800

    [REV] add p25 solution

--- src/main/java/com/feyfree/algs4/Heap.java
+++ src/main/java/com/feyfree/algs4/Heap.java
@@ -4,7 +4,7 @@
  *  Dependencies: StdOut.java StdIn.java
  *  Data files:   https://algs4.cs.princeton.edu/24pq/tiny.txt
  *                https://algs4.cs.princeton.edu/24pq/words3.txt
- *  
+ *
  *  Sorts a sequence of strings from standard input using heapsort.
  *
  *  % more tiny.txt
@@ -24,39 +24,42 @@
 package com.feyfree.algs4;
 
 /**
- *  The {@code Heap} class provides a static method to sort an array
- *  using <em>heapsort</em>.
- *  <p>
- *  This implementation takes &Theta;(<em>n</em> log <em>n</em>) time
- *  to sort any array of length <em>n</em> (assuming comparisons
- *  take constant time). It makes at most 
- *  2 <em>n</em> log<sub>2</sub> <em>n</em> compares.
- *  <p>
- *  This sorting algorithm is not stable.
- *  It uses &Theta;(1) extra memory (not including the input array).
- *  <p>
- *  For additional documentation, see
- *  <a href="https://algs4.cs.princeton.edu/24pq">Section 2.4</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The {@code Heap} class provides a static method to sort an array
+ * using <em>heapsort</em>.
+ * <p>
+ * This implementation takes &Theta;(<em>n</em> log <em>n</em>) time
+ * to sort any array of length <em>n</em> (assuming comparisons
+ * take constant time). It makes at most
+ * 2 <em>n</em> log<sub>2</sub> <em>n</em> compares.
+ * <p>
+ * This sorting algorithm is not stable.
+ * It uses &Theta;(1) extra memory (not including the input array).
+ * <p>
+ * For additional documentation, see
+ * <a href="https://algs4.cs.princeton.edu/24pq">Section 2.4</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
 public class Heap {
 
     // This class should not be instantiated.
-    private Heap() { }
+    private Heap() {
+    }
 
     /**
      * Rearranges the array in ascending order, using the natural order.
+     *
      * @param pq the array to be sorted
      */
     public static void sort(Comparable[] pq) {
         int n = pq.length;
 
         // heapify phase
-        for (int k = n/2; k >= 1; k--)
+        for (int k = n / 2; k >= 1; k--) {
             sink(pq, k, n);
+        }
 
         // sortdown phase
         int k = n;
@@ -66,32 +69,36 @@ public class Heap {
         }
     }
 
-   /***************************************************************************
-    * Helper functions to restore the heap invariant.
-    ***************************************************************************/
+    /***************************************************************************
+     * Helper functions to restore the heap invariant.
+     ***************************************************************************/
 
     private static void sink(Comparable[] pq, int k, int n) {
-        while (2*k <= n) {
-            int j = 2*k;
-            if (j < n && less(pq, j, j+1)) j++;
-            if (!less(pq, k, j)) break;
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && less(pq, j, j + 1)) {
+                j++;
+            }
+            if (!less(pq, k, j)) {
+                break;
+            }
             exch(pq, k, j);
             k = j;
         }
     }
 
-   /***************************************************************************
-    * Helper functions for comparisons and swaps.
-    * Indices are "off-by-one" to support 1-based indexing.
-    ***************************************************************************/
+    /***************************************************************************
+     * Helper functions for comparisons and swaps.
+     * Indices are "off-by-one" to support 1-based indexing.
+     ***************************************************************************/
     private static boolean less(Comparable[] pq, int i, int j) {
-        return pq[i-1].compareTo(pq[j-1]) < 0;
+        return pq[i - 1].compareTo(pq[j - 1]) < 0;
     }
 
     private static void exch(Object[] pq, int i, int j) {
-        Object swap = pq[i-1];
-        pq[i-1] = pq[j-1];
-        pq[j-1] = swap;
+        Object swap = pq[i - 1];
+        pq[i - 1] = pq[j - 1];
+        pq[j - 1] = swap;
     }
 
     // print array to standard output
@@ -102,8 +109,8 @@ public class Heap {
     }
 
     /**
-     * Reads in a sequence of strings from standard input; heapsorts them; 
-     * and prints them to standard output in ascending order. 
+     * Reads in a sequence of strings from standard input; heapsorts them;
+     * and prints them to standard output in ascending order.
      *
      * @param args the command-line arguments
      */
--- src/main/java/com/feyfree/leetcode/p25/Solution.java
+++ src/main/java/com/feyfree/leetcode/p25/Solution.java
@@ -1,9 +1,48 @@
 package com.feyfree.leetcode.p25;
 
 
+import com.feyfree.leetcode.commons.ListNode;
+
 /**
+ * K 个一组翻转链表
+ * reverse-nodes-in-k-group
+ * <p>
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * <p>
+ * k 是一个正整数，它的值小于或等于链表的长度。
+ * <p>
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  * @author leilei
  */
 public class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k <= 1) {
+            return head;
+        }
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        int len = 1;
+        while (head.next != null) {
+            len += 1;
+            head = head.next;
+        }
+        ListNode pre = dummy;
+        for (int i = 0; i + k <= len; i += k) {
+            ListNode cur = pre.next;
+            ListNode nxt = cur.next;
+            for (int j = 1; j < k; j++) {
+                cur.next = nxt.next;
+                nxt.next = pre.next;
+                pre.next = nxt;
+                nxt = cur.next;
+            }
+            pre = cur;
+        }
+        return dummy.next;
+    }
 }
