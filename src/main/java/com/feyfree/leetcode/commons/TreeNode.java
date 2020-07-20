fa944927c8cd9f241b611213046d311b6b5559f1
leilei <gengll2@yunjiglobal.com>
2020-07-20 17:50:57 +0800

    [REV] add p450 solution delete node in BST

---
+++ src/main/java/com/feyfree/leetcode/commons/TreeNode.java
@@ -0,0 +1,12 @@
+package com.feyfree.leetcode.commons;
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
