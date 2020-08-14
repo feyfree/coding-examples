8b54e559dd2b3b84d7f600529e0d934bb4fc8d87
leilei <feyfree@163.com>
2020-08-14 18:17:29 +0800

    [REV] add p39 solution template

---
+++ src/main/java/com/feyfree/leetcode/p39/Solution.java
@@ -0,0 +1,48 @@
+package com.feyfree.leetcode.p39;
+
+/**
+ * 39. 组合总和
+ * <p>
+ * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ * <p>
+ * candidates 中的数字可以无限制重复被选取。
+ * <p>
+ * 说明：
+ * <p>
+ * 所有数字（包括 target）都是正整数。
+ * 解集不能包含重复的组合。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：candidates = [2,3,6,7], target = 7,
+ * 所求解集为：
+ * [
+ * [7],
+ * [2,2,3]
+ * ]
+ * 示例 2：
+ * <p>
+ * 输入：candidates = [2,3,5], target = 8,
+ * 所求解集为：
+ * [
+ *   [2,2,2,2],
+ *   [2,3,3],
+ *   [3,5]
+ * ]
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= candidates.length <= 30
+ * 1 <= candidates[i] <= 200
+ * candidate 中的每个元素都是独一无二的。
+ * 1 <= target <= 500
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/combination-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author leilei
+ * @date 2020/8/14 17:38
+ */
+public class Solution {
+}
