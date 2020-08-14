d5729f2dca3c9de89ebb08646611c7edbf05642e
leilei <feyfree@163.com>
2020-08-14 09:23:02 +0800

    [REV] add solution comment

--- src/main/java/com/feyfree/leetcode/p785/Solution.java
+++ src/main/java/com/feyfree/leetcode/p785/Solution.java
@@ -41,6 +41,9 @@ package com.feyfree.leetcode.p785;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/is-graph-bipartite
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * <p>
+ * <p>
+ * 题解: DFS + 染色
  *
  * @author leilei
  */
