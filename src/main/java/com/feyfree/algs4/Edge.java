177ec67a71a64ec760ed3996ade3b2ead02da77a
leilei <feyfree@163.com>
2020-08-24 14:04:13 +0800

    [ADD] add skip list data structure

--- src/main/java/com/feyfree/algs4/Edge.java
+++ src/main/java/com/feyfree/algs4/Edge.java
@@ -10,20 +10,20 @@
 package com.feyfree.algs4;
 
 /**
- *  The {@code Edge} class represents a weighted edge in an 
- *  {@link EdgeWeightedGraph}. Each edge consists of two integers
- *  (naming the two vertices) and a real-value weight. The data type
- *  provides methods for accessing the two endpoints of the edge and
- *  the weight. The natural order for this data type is by
- *  ascending order of weight.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/43mst">Section 4.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The {@code Edge} class represents a weighted edge in an
+ * {@link EdgeWeightedGraph}. Each edge consists of two integers
+ * (naming the two vertices) and a real-value weight. The data type
+ * provides methods for accessing the two endpoints of the edge and
+ * the weight. The natural order for this data type is by
+ * ascending order of weight.
+ * <p>
+ * For additional documentation, see <a href="https://algs4.cs.princeton.edu/43mst">Section 4.3</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
-public class Edge implements Comparable<Edge> { 
+public class Edge implements Comparable<Edge> {
 
     private final int v;
     private final int w;
@@ -33,17 +33,23 @@ public class Edge implements Comparable<Edge> {
      * Initializes an edge between vertices {@code v} and {@code w} of
      * the given {@code weight}.
      *
-     * @param  v one vertex
-     * @param  w the other vertex
-     * @param  weight the weight of this edge
-     * @throws IllegalArgumentException if either {@code v} or {@code w} 
-     *         is a negative integer
+     * @param v      one vertex
+     * @param w      the other vertex
+     * @param weight the weight of this edge
+     * @throws IllegalArgumentException if either {@code v} or {@code w}
+     *                                  is a negative integer
      * @throws IllegalArgumentException if {@code weight} is {@code NaN}
      */
     public Edge(int v, int w, double weight) {
-        if (v < 0) throw new IllegalArgumentException("vertex index must be a nonnegative integer");
-        if (w < 0) throw new IllegalArgumentException("vertex index must be a nonnegative integer");
-        if (Double.isNaN(weight)) throw new IllegalArgumentException("Weight is NaN");
+        if (v < 0) {
+            throw new IllegalArgumentException("vertex index must be a nonnegative integer");
+        }
+        if (w < 0) {
+            throw new IllegalArgumentException("vertex index must be a nonnegative integer");
+        }
+        if (Double.isNaN(weight)) {
+            throw new IllegalArgumentException("Weight is NaN");
+        }
         this.v = v;
         this.w = w;
         this.weight = weight;
@@ -70,15 +76,19 @@ public class Edge implements Comparable<Edge> {
     /**
      * Returns the endpoint of this edge that is different from the given vertex.
      *
-     * @param  vertex one endpoint of this edge
+     * @param vertex one endpoint of this edge
      * @return the other endpoint of this edge
      * @throws IllegalArgumentException if the vertex is not one of the
-     *         endpoints of this edge
+     *                                  endpoints of this edge
      */
     public int other(int vertex) {
-        if      (vertex == v) return w;
-        else if (vertex == w) return v;
-        else throw new IllegalArgumentException("Illegal endpoint");
+        if (vertex == v) {
+            return w;
+        } else if (vertex == w) {
+            return v;
+        } else {
+            throw new IllegalArgumentException("Illegal endpoint");
+        }
     }
 
     /**
@@ -86,10 +96,10 @@ public class Edge implements Comparable<Edge> {
      * Note that {@code compareTo()} is not consistent with {@code equals()},
      * which uses the reference equality implementation inherited from {@code Object}.
      *
-     * @param  that the other edge
+     * @param that the other edge
      * @return a negative integer, zero, or positive integer depending on whether
-     *         the weight of this is less than, equal to, or greater than the
-     *         argument edge
+     * the weight of this is less than, equal to, or greater than the
+     * argument edge
      */
     @Override
     public int compareTo(Edge that) {
@@ -101,6 +111,7 @@ public class Edge implements Comparable<Edge> {
      *
      * @return a string representation of this edge
      */
+    @Override
     public String toString() {
         return String.format("%d-%d %.5f", v, w, weight);
     }
