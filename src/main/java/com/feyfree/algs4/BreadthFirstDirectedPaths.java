f4894db761bdccde1476b882bcc523a7738fc3b8
leilei <feyfree@163.com>
2020-08-25 18:06:55 +0800

    [ADD] add jump game solution template

--- src/main/java/com/feyfree/algs4/BreadthFirstDirectedPaths.java
+++ src/main/java/com/feyfree/algs4/BreadthFirstDirectedPaths.java
@@ -29,23 +29,23 @@
 package com.feyfree.algs4;
 
 /**
- *  The {@code BreadthDirectedFirstPaths} class represents a data type for
- *  finding shortest paths (number of edges) from a source vertex <em>s</em>
- *  (or set of source vertices) to every other vertex in the digraph.
- *  <p>
- *  This implementation uses breadth-first search.
- *  The constructor takes &Theta;(<em>V</em> + <em>E</em>) time in the
- *  worst case, where <em>V</em> is the number of vertices and <em>E</em> is
- *  the number of edges.
- *  Each instance method takes &Theta;(1) time.
- *  It uses &Theta;(<em>V</em>) extra space (not including the digraph).
- *  <p>
- *  For additional documentation, 
- *  see <a href="https://algs4.cs.princeton.edu/42digraph">Section 4.2</a> of 
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The {@code BreadthDirectedFirstPaths} class represents a data type for
+ * finding shortest paths (number of edges) from a source vertex <em>s</em>
+ * (or set of source vertices) to every other vertex in the digraph.
+ * <p>
+ * This implementation uses breadth-first search.
+ * The constructor takes &Theta;(<em>V</em> + <em>E</em>) time in the
+ * worst case, where <em>V</em> is the number of vertices and <em>E</em> is
+ * the number of edges.
+ * Each instance method takes &Theta;(1) time.
+ * It uses &Theta;(<em>V</em>) extra space (not including the digraph).
+ * <p>
+ * For additional documentation,
+ * see <a href="https://algs4.cs.princeton.edu/42digraph">Section 4.2</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
 public class BreadthFirstDirectedPaths {
     private static final int INFINITY = Integer.MAX_VALUE;
@@ -55,6 +55,7 @@ public class BreadthFirstDirectedPaths {
 
     /**
      * Computes the shortest path from {@code s} and every other vertex in graph {@code G}.
+     *
      * @param G the digraph
      * @param s the source vertex
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
@@ -63,8 +64,9 @@ public class BreadthFirstDirectedPaths {
         marked = new boolean[G.V()];
         distTo = new int[G.V()];
         edgeTo = new int[G.V()];
-        for (int v = 0; v < G.V(); v++)
+        for (int v = 0; v < G.V(); v++) {
             distTo[v] = INFINITY;
+        }
         validateVertex(s);
         bfs(G, s);
     }
@@ -72,18 +74,20 @@ public class BreadthFirstDirectedPaths {
     /**
      * Computes the shortest path from any one of the source vertices in {@code sources}
      * to every other vertex in graph {@code G}.
-     * @param G the digraph
+     *
+     * @param G       the digraph
      * @param sources the source vertices
      * @throws IllegalArgumentException if {@code sources} is {@code null}
      * @throws IllegalArgumentException unless each vertex {@code v} in
-     *         {@code sources} satisfies {@code 0 <= v < V}
+     *                                  {@code sources} satisfies {@code 0 <= v < V}
      */
     public BreadthFirstDirectedPaths(Digraph G, Iterable<Integer> sources) {
         marked = new boolean[G.V()];
         distTo = new int[G.V()];
         edgeTo = new int[G.V()];
-        for (int v = 0; v < G.V(); v++)
+        for (int v = 0; v < G.V(); v++) {
             distTo[v] = INFINITY;
+        }
         validateVertices(sources);
         bfs(G, sources);
     }
@@ -130,6 +134,7 @@ public class BreadthFirstDirectedPaths {
 
     /**
      * Is there a directed path from the source {@code s} (or sources) to vertex {@code v}?
+     *
      * @param v the vertex
      * @return {@code true} if there is a directed path, {@code false} otherwise
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
@@ -142,6 +147,7 @@ public class BreadthFirstDirectedPaths {
     /**
      * Returns the number of edges in a shortest path from the source {@code s}
      * (or sources) to vertex {@code v}?
+     *
      * @param v the vertex
      * @return the number of edges in a shortest path
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
@@ -154,6 +160,7 @@ public class BreadthFirstDirectedPaths {
     /**
      * Returns a shortest path from {@code s} (or sources) to {@code v}, or
      * {@code null} if no such path.
+     *
      * @param v the vertex
      * @return the sequence of vertices on a shortest path, as an Iterable
      * @throws IllegalArgumentException unless {@code 0 <= v < V}
@@ -161,11 +168,14 @@ public class BreadthFirstDirectedPaths {
     public Iterable<Integer> pathTo(int v) {
         validateVertex(v);
 
-        if (!hasPathTo(v)) return null;
+        if (!hasPathTo(v)) {
+            return null;
+        }
         Stack<Integer> path = new Stack<Integer>();
         int x;
-        for (x = v; distTo[x] != 0; x = edgeTo[x])
+        for (x = v; distTo[x] != 0; x = edgeTo[x]) {
             path.push(x);
+        }
         path.push(x);
         return path;
     }
@@ -173,8 +183,9 @@ public class BreadthFirstDirectedPaths {
     // throw an IllegalArgumentException unless {@code 0 <= v < V}
     private void validateVertex(int v) {
         int V = marked.length;
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+        if (v < 0 || v >= V) {
+            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
+        }
     }
 
     // throw an IllegalArgumentException unless {@code 0 <= v < V}
@@ -208,13 +219,14 @@ public class BreadthFirstDirectedPaths {
             if (bfs.hasPathTo(v)) {
                 StdOut.printf("%d to %d (%d):  ", s, v, bfs.distTo(v));
                 for (int x : bfs.pathTo(v)) {
-                    if (x == s) StdOut.print(x);
-                    else        StdOut.print("->" + x);
+                    if (x == s) {
+                        StdOut.print(x);
+                    } else {
+                        StdOut.print("->" + x);
+                    }
                 }
                 StdOut.println();
-            }
-
-            else {
+            } else {
                 StdOut.printf("%d to %d (-):  not connected\n", s, v);
             }
 
