5f57d8ddcbc453b2d9107202d72a92a4f1423dce
leilei <feyfree@163.com>
2020-08-26 18:16:14 +0800

    [ADD] add p50 solution template

---
+++ src/main/java/com/feyfree/leetcode/p50/Solution.java
@@ -0,0 +1,28 @@
+package com.feyfree.leetcode.p50;
+
+
+/**
+ * https://leetcode-cn.com/problems/powx-n/
+ * <p>
+ * 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+ * <p>
+ * 输入: 2.00000, 10
+ * 输出: 1024.00000
+ * <p>
+ * 输入: 2.10000, 3
+ * 输出: 9.26100
+ * <p>
+ * 输入: 2.00000, -2
+ * 输出: 0.25000
+ * 解释: 2-2 = 1/22 = 1/4 = 0.25
+ *
+ * @author leilei
+ */
+public class Solution {
+
+    public double myPow(double x, int n) {
+        return 0.0;
+    }
+
+
+}
