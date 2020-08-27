602263a9bc104d20b82823b40766a0f500e70076
leilei <feyfree@163.com>
2020-08-27 18:01:58 +0800

    [REV] add p50 solution

--- src/main/java/com/feyfree/leetcode/p50/Solution.java
+++ src/main/java/com/feyfree/leetcode/p50/Solution.java
@@ -14,14 +14,32 @@ package com.feyfree.leetcode.p50;
  * <p>
  * 输入: 2.00000, -2
  * 输出: 0.25000
- * 解释: 2-2 = 1/22 = 1/4 = 0.25
  *
  * @author leilei
  */
 public class Solution {
 
     public double myPow(double x, int n) {
-        return 0.0;
+        return n >= 0 ? myPowImpl(x, n) : 1.0 / myPowImpl(x, n);
+    }
+
+    /**
+     * 递归的思想
+     *
+     * @param x 输入
+     * @param n 幂
+     * @return 输出
+     */
+    private double myPowImpl(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return myPowImpl(x * x, n / 2) * (n % 2 == 0 ? x : 1);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.myPow(0, 0));
     }
 
 
