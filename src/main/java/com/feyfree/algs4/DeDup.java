af295ef49a9c8c906f2a212f0d1cc6acc430f08f
leilei <feyfree@163.com>
2020-09-10 11:51:44 +0800

    [REV] format code

--- src/main/java/com/feyfree/algs4/DeDup.java
+++ src/main/java/com/feyfree/algs4/DeDup.java
@@ -33,23 +33,24 @@
 package com.feyfree.algs4;
 
 /**
- *  The {@code DeDup} class provides a client for reading in a sequence of
- *  words from standard input and printing each word, removing any duplicates.
- *  It is useful as a test client for various symbol table implementations.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/35applications">Section 3.5</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The {@code DeDup} class provides a client for reading in a sequence of
+ * words from standard input and printing each word, removing any duplicates.
+ * It is useful as a test client for various symbol table implementations.
+ * <p>
+ * For additional documentation, see <a href="https://algs4.cs.princeton.edu/35applications">Section 3.5</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
-public class DeDup {  
+public class DeDup {
 
     // Do not instantiate.
-    private DeDup() { }
+    private DeDup() {
+    }
 
     public static void main(String[] args) {
-        SET<String> set = new SET<String>();
+        SET<String> set = new SET<>();
 
         // read in strings and add to set
         while (!StdIn.isEmpty()) {
