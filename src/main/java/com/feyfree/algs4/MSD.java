a105cf298ce368fd84a11f08a59a64d3bcb2995b
leilei <feyfree@163.com>
2020-09-11 17:37:16 +0800

    [REV] add leetcode 57 solution

--- src/main/java/com/feyfree/algs4/MSD.java
+++ src/main/java/com/feyfree/algs4/MSD.java
@@ -28,26 +28,27 @@
 package com.feyfree.algs4;
 
 /**
- *  The {@code MSD} class provides static methods for sorting an
- *  array of extended ASCII strings or integers using MSD radix sort.
- *  <p>
- *  For additional documentation,
- *  see <a href="https://algs4.cs.princeton.edu/51radix">Section 5.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The {@code MSD} class provides static methods for sorting an
+ * array of extended ASCII strings or integers using MSD radix sort.
+ * <p>
+ * For additional documentation,
+ * see <a href="https://algs4.cs.princeton.edu/51radix">Section 5.1</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
 public class MSD {
-    private static final int BITS_PER_BYTE =   8;
-    private static final int BITS_PER_INT  =  32;   // each Java int is 32 bits 
-    private static final int R             = 256;   // extended ASCII alphabet size
-    private static final int CUTOFF        =  15;   // cutoff to insertion sort
+    private static final int BITS_PER_BYTE = 8;
+    private static final int BITS_PER_INT = 32;   // each Java int is 32 bits
+    private static final int R = 256;   // extended ASCII alphabet size
+    private static final int CUTOFF = 15;   // cutoff to insertion sort
 
     // do not instantiate
-    private MSD() { } 
+    private MSD() {
+    }
 
-   /**
+    /**
      * Rearranges the array of extended ASCII strings in ascending order.
      *
      * @param a the array to be sorted
@@ -55,13 +56,15 @@ public class MSD {
     public static void sort(String[] a) {
         int n = a.length;
         String[] aux = new String[n];
-        sort(a, 0, n-1, 0, aux);
+        sort(a, 0, n - 1, 0, aux);
     }
 
     // return dth character of s, -1 if d = length of string
     private static int charAt(String s, int d) {
         assert d >= 0 && d <= s.length();
-        if (d == s.length()) return -1;
+        if (d == s.length()) {
+            return -1;
+        }
         return s.charAt(d);
     }
 
@@ -75,38 +78,43 @@ public class MSD {
         }
 
         // compute frequency counts
-        int[] count = new int[R+2];
+        int[] count = new int[R + 2];
         for (int i = lo; i <= hi; i++) {
             int c = charAt(a[i], d);
-            count[c+2]++;
+            count[c + 2]++;
         }
 
         // transform counts to indicies
-        for (int r = 0; r < R+1; r++)
-            count[r+1] += count[r];
+        for (int r = 0; r < R + 1; r++) {
+            count[r + 1] += count[r];
+        }
 
         // distribute
         for (int i = lo; i <= hi; i++) {
             int c = charAt(a[i], d);
-            aux[count[c+1]++] = a[i];
+            aux[count[c + 1]++] = a[i];
         }
 
         // copy back
-        for (int i = lo; i <= hi; i++) 
+        for (int i = lo; i <= hi; i++) {
             a[i] = aux[i - lo];
+        }
 
 
         // recursively sort for each character (excludes sentinel -1)
-        for (int r = 0; r < R; r++)
-            sort(a, lo + count[r], lo + count[r+1] - 1, d+1, aux);
+        for (int r = 0; r < R; r++) {
+            sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1, aux);
+        }
     }
 
 
     // insertion sort a[lo..hi], starting at dth character
     private static void insertion(String[] a, int lo, int hi, int d) {
-        for (int i = lo; i <= hi; i++)
-            for (int j = i; j > lo && less(a[j], a[j-1], d); j--)
-                exch(a, j, j-1);
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j - 1], d); j--) {
+                exch(a, j, j - 1);
+            }
+        }
     }
 
     // exchange a[i] and a[j]
@@ -120,14 +128,18 @@ public class MSD {
     private static boolean less(String v, String w, int d) {
         // assert v.substring(0, d).equals(w.substring(0, d));
         for (int i = d; i < Math.min(v.length(), w.length()); i++) {
-            if (v.charAt(i) < w.charAt(i)) return true;
-            if (v.charAt(i) > w.charAt(i)) return false;
+            if (v.charAt(i) < w.charAt(i)) {
+                return true;
+            }
+            if (v.charAt(i) > w.charAt(i)) {
+                return false;
+            }
         }
         return v.length() < w.length();
     }
 
 
-   /**
+    /**
      * Rearranges the array of 32-bit integers in ascending order.
      * Currently assumes that the integers are nonnegative.
      *
@@ -136,7 +148,7 @@ public class MSD {
     public static void sort(int[] a) {
         int n = a.length;
         int[] aux = new int[n];
-        sort(a, 0, n-1, 0, aux);
+        sort(a, 0, n - 1, 0, aux);
     }
 
     // MSD sort from a[lo] to a[hi], starting at the dth byte
@@ -149,29 +161,30 @@ public class MSD {
         }
 
         // compute frequency counts (need R = 256)
-        int[] count = new int[R+1];
+        int[] count = new int[R + 1];
         int mask = R - 1;   // 0xFF;
-        int shift = BITS_PER_INT - BITS_PER_BYTE*d - BITS_PER_BYTE;
+        int shift = BITS_PER_INT - BITS_PER_BYTE * d - BITS_PER_BYTE;
         for (int i = lo; i <= hi; i++) {
             int c = (a[i] >> shift) & mask;
             count[c + 1]++;
         }
 
         // transform counts to indicies
-        for (int r = 0; r < R; r++)
-            count[r+1] += count[r];
+        for (int r = 0; r < R; r++) {
+            count[r + 1] += count[r];
+        }
 
 /************* BUGGGY CODE.
-        // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
-        if (d == 0) {
-            int shift1 = count[R] - count[R/2];
-            int shift2 = count[R/2];
-            for (int r = 0; r < R/2; r++)
-                count[r] += shift1;
-            for (int r = R/2; r < R; r++)
-                count[r] -= shift2;
-        }
-************************************/
+ // for most significant byte, 0x80-0xFF comes before 0x00-0x7F
+ if (d == 0) {
+ int shift1 = count[R] - count[R/2];
+ int shift2 = count[R/2];
+ for (int r = 0; r < R/2; r++)
+ count[r] += shift1;
+ for (int r = R/2; r < R; r++)
+ count[r] -= shift2;
+ }
+ ************************************/
         // distribute
         for (int i = lo; i <= hi; i++) {
             int c = (a[i] >> shift) & mask;
@@ -179,25 +192,33 @@ public class MSD {
         }
 
         // copy back
-        for (int i = lo; i <= hi; i++) 
+        for (int i = lo; i <= hi; i++) {
             a[i] = aux[i - lo];
+        }
 
         // no more bits
-        if (d == 4) return;
+        if (d == 4) {
+            return;
+        }
 
         // recursively sort for each character
-        if (count[0] > 0)
-            sort(a, lo, lo + count[0] - 1, d+1, aux);
-        for (int r = 0; r < R; r++)
-            if (count[r+1] > count[r])
-                sort(a, lo + count[r], lo + count[r+1] - 1, d+1, aux);
+        if (count[0] > 0) {
+            sort(a, lo, lo + count[0] - 1, d + 1, aux);
+        }
+        for (int r = 0; r < R; r++) {
+            if (count[r + 1] > count[r]) {
+                sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1, aux);
+            }
+        }
     }
 
     // TODO: insertion sort a[lo..hi], starting at dth character
     private static void insertion(int[] a, int lo, int hi, int d) {
-        for (int i = lo; i <= hi; i++)
-            for (int j = i; j > lo && a[j] < a[j-1]; j--)
-                exch(a, j, j-1);
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && a[j] < a[j - 1]; j--) {
+                exch(a, j, j - 1);
+            }
+        }
     }
 
     // exchange a[i] and a[j]
@@ -219,8 +240,9 @@ public class MSD {
         String[] a = StdIn.readAllStrings();
         int n = a.length;
         sort(a);
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             StdOut.println(a[i]);
+        }
     }
 }
 
--- src/main/java/com/feyfree/leetcode/p57/Solution.java
+++ src/main/java/com/feyfree/leetcode/p57/Solution.java
@@ -1,26 +1,46 @@
 package com.feyfree.leetcode.p57;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
- * p57 插入区间 解法跟合并区间类似
+ * 57
+ * <p>
+ * 给出一个无重叠的 ，按照区间起始端点排序的区间列表。
+ * 在列表中插入一个新的区间，你需要确保列表中的区间仍然有序且不重叠（如果有必要的话，可以合并区间）。
+ * <p>
+ * 用java 处理多维数组太麻烦  这个利用 56 题的结论操作
  *
  * @author leilei
  */
-class Solution {
+public class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        // 首先是排序 按照 start 升序排列
-        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
-        // 构建一个链表
-        List<List<Integer>> result = new LinkedList<>();
+        List<int[]> data = new LinkedList<>();
+        List<int[]> result = new LinkedList<>();
+        // 将 数组转化成 列表 进行复制
         for (int[] interval : intervals) {
-            List<Integer> data = new ArrayList<>(2);
-            data.add(interval[0]);
-            data.add(interval[1]);
-            result.add(data);
+            // 判断什么时候插入new interval
+            if (interval[0] >= newInterval[0]) {
+                // 先插入 newInterval
+                data.add(newInterval);
+            }
+            data.add(interval);
         }
-        return null;
-
-
+        if (data.size() == intervals.length) {
+            data.add(newInterval);
+        }
+        for (int[] interval : data) {
+            if (result.size() == 0 || interval[0] > result.get(result.size() - 1)[1]) {
+                result.add(interval);
+            } else {
+                result.get(result.size() - 1)[1] =  Math.max(result.get(result.size() - 1)[1], interval[1]);
+            }
+        }
+        // list to array
+        int[][] output = new int[result.size()][2];
+        for (int i = 0; i < result.size(); i++) {
+            output[i] = result.get(i);
+        }
+        return output;
     }
 }
