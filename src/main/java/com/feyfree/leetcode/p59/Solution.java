ef3b6533aa97791b38beda696cc652322d75320e
leilei <feyfree@163.com>
2020-09-14 16:17:28 +0800

    [REV] add leetcode 59 solution

--- src/main/java/com/feyfree/leetcode/p59/Solution.java
+++ src/main/java/com/feyfree/leetcode/p59/Solution.java
@@ -1,5 +1,7 @@
 package com.feyfree.leetcode.p59;
 
+import java.util.Arrays;
+
 /**
  * 59. 螺旋矩阵 II
  * <p>
@@ -18,6 +20,66 @@ package com.feyfree.leetcode.p59;
 public class Solution {
     public int[][] generateMatrix(int n) {
         int[][] result = new int[n][n];
+        int total = n * n;
+        // 当前赋值
+        int current = 1;
+        // 操作次数
+        int times = 0;
+        --n;
+        // 标明是哪个维度的
+        int d = 0;
+        // 起点 x
+        int x = 0;
+        // 起点 y
+        int y = 0;
+
+        // left
+        int l = 0;
+        // top
+        int t = 0;
+        // right
+        int r = n;
+        // bottom
+        int b = n;
+
+        while (times < total) {
+            times += 1;
+            // 将current 放进指定 的 x, y 中
+            // 计算x, y
+            if (d == 0) {
+                while (x < r) {
+                    result[y][x++] = current;
+                    current += 1;
+                }
+                ++t;
+            } else if (d == 1) {
+                while (y < b) {
+                    result[y++][x] = current;
+                    current += 1;
+                }
+                --r;
+            } else if (d == 2) {
+                while (x > l) {
+                    result[y][x--] = current;
+                    current += 1;
+                }
+                --b;
+            } else {
+                while (y > t) {
+                    result[y--][x] = current;
+                    current += 1;
+                }
+                ++l;
+            }
+            d = (d + 1) % 4;
+        }
+        result[y][x] = total;
         return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] result = solution.generateMatrix(3);
+        System.out.println(Arrays.deepToString(result));
     }
 }
