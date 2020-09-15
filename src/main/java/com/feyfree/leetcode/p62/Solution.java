abc54858a0c6e87566a4e280ac23de5d934da37c
leilei <feyfree@163.com>
2020-09-15 18:04:04 +0800

    [REV] add leetcode solution template

---
+++ src/main/java/com/feyfree/leetcode/p62/Solution.java
@@ -0,0 +1,20 @@
+package com.feyfree.leetcode.p62;
+
+/**
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * <p>
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * <p>
+ * 问总共有多少条不同的路径？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-paths
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author leilei
+ */
+public class Solution {
+    public int uniquePaths(int m, int n) {
+        return 0;
+    }
+}
