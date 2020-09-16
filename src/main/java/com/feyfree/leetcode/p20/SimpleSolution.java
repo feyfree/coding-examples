5ba14b8b34b3f266735a2d9d1294a51a9d4c8fe9
leilei <gengll2@yunjiglobal.com>
2020-09-16 23:36:00 +0800

    [ADD] leetcode 20 simple solution

---
+++ src/main/java/com/feyfree/leetcode/p20/SimpleSolution.java
@@ -0,0 +1,30 @@
+package com.feyfree.leetcode.p20;
+
+import redis.clients.jedis.ZParams;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ * 简化版本
+ *
+ * @author leilei
+ */
+public class SimpleSolution {
+    public boolean isValid(String s) {
+        Stack<Character> result = new Stack<>();
+        HashMap<Character, Character> parenMap = new HashMap<>(4);
+        parenMap.put(')', '(');
+        parenMap.put(']', '[');
+        parenMap.put('}', '{');
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!parenMap.containsKey(c)) {
+                result.push(c);
+            } else if (result.isEmpty() || !parenMap.get(c).equals(result.pop())) {
+                return false;
+            }
+        }
+        return result.isEmpty();
+    }
+}
