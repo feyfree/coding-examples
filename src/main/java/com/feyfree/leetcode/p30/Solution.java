0e1c369260c2fca23214187673a5429e44006035
leilei <feyfree@163.com>
2020-09-23 18:51:38 +0800

    [ADD] add leetcode 30 solution template

---
+++ src/main/java/com/feyfree/leetcode/p30/Solution.java
@@ -0,0 +1,15 @@
+package com.feyfree.leetcode.p30;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * leetcode 30 solution
+ *
+ * @author leilei
+ */
+public class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        return new ArrayList<>();
+    }
+}
