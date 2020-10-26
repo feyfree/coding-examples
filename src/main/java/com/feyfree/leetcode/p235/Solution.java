d97a68e82b278afb9bd0db06b545ba0440f0ccdd
leilei <feyfree@163.com>
2020-10-26 15:16:45 +0800

    [REV] 增加leetcode 二叉树解法

---
+++ src/main/java/com/feyfree/leetcode/p235/Solution.java
@@ -0,0 +1,24 @@
+package com.feyfree.leetcode.p235;
+
+import com.feyfree.leetcode.commons.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ * <p>
+ * 235  二叉搜索树
+ *
+ * @author leilei
+ */
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+
+}
