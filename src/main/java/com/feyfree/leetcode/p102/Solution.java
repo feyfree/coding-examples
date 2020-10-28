594ced90e4ebd6b525c2db858a2d2224acf57a76
leilei <gengll2@yunjiglobal.com>
2020-10-28 23:14:55 +0800

    [ADD] add 102 solution template

---
+++ src/main/java/com/feyfree/leetcode/p102/Solution.java
@@ -0,0 +1,24 @@
+package com.feyfree.leetcode.p102;
+
+import com.feyfree.leetcode.commons.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）
+ *
+ * @author leilei
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        return null;
+    }
+}
