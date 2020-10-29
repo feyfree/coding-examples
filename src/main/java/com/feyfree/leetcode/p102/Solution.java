e7bc8275d37101997eda49edb5c17e065778684a
leilei <feyfree@163.com>
2020-10-29 09:56:16 +0800

    [ADD] add leetcode 122 bfs solution

--- src/main/java/com/feyfree/leetcode/p102/Solution.java
+++ src/main/java/com/feyfree/leetcode/p102/Solution.java
@@ -2,13 +2,15 @@ package com.feyfree.leetcode.p102;
 
 import com.feyfree.leetcode.commons.TreeNode;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 /**
- * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）
+ * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （
+ * 即逐层地，从左到右访问所有节点）
+ *
+ * https://zxi.mytechroad.com/blog/leetcode/leetcode-102-binary-tree-level-order-traversal/
+ * <p>
+ * BFS solution
  *
  * @author leilei
  */
@@ -18,7 +20,23 @@ class Solution {
             return new ArrayList<>();
         }
         List<List<Integer>> result = new ArrayList<>();
-        Queue<Integer> queue = new LinkedList<>();
-        return null;
+        List<TreeNode> curr = new ArrayList<>();
+        List<TreeNode> next = new ArrayList<>();
+        curr.add(root);
+        while (!curr.isEmpty()) {
+            result.add(new ArrayList<>());
+            for (TreeNode node : curr) {
+                result.get(result.size() - 1).add(node.val);
+                if (node.left != null) {
+                    next.add(node.left);
+                }
+                if (node.right != null) {
+                    next.add(node.right);
+                }
+            }
+            curr = new ArrayList<>(next);
+            next.clear();
+        }
+        return result;
     }
 }
