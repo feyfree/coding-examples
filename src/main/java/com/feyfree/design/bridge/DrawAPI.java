2df3423d4be074201741d2189715768d1a82a475
leilei <feyfree@163.com>
2020-11-01 13:59:42 +0800

    [ADD] 增加bridge 模式

---
+++ src/main/java/com/feyfree/design/bridge/DrawAPI.java
@@ -0,0 +1,5 @@
+package com.feyfree.design.bridge;
+
+public interface DrawAPI {
+    void drawCircle(int radius, int x, int y);
+}
---
+++ src/main/java/com/feyfree/design/bridge/RedCircle.java
@@ -0,0 +1,9 @@
+package com.feyfree.design.bridge;
+
+public class RedCircle implements DrawAPI {
+    @Override
+    public void drawCircle(int radius, int x, int y) {
+        System.out.println("Drawing Circle[ color: red, radius: "
+                + radius + ", x: " + x + ", " + y + "]");
+    }
+}
