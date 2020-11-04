a5234f1f7084af9104478d53ba9e63c489e9a853
leilei <feyfree@163.com>
2020-11-04 16:08:56 +0800

    [REV] add leetcode 69 solution

--- src/main/java/com/feyfree/algs4/BinarySearch.java
+++ src/main/java/com/feyfree/algs4/BinarySearch.java
@@ -20,7 +20,7 @@
  *  140925
  *  161828
  *  [367,966 total values]
- *  
+ *
  ******************************************************************************/
 
 package com.feyfree.algs4;
@@ -28,29 +28,30 @@ package com.feyfree.algs4;
 import java.util.Arrays;
 
 /**
- *  The {@code BinarySearch} class provides a static method for binary
- *  searching for an integer in a sorted array of integers.
- *  <p>
- *  The <em>indexOf</em> operations takes logarithmic time in the worst case.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/11model">Section 1.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * The {@code BinarySearch} class provides a static method for binary
+ * searching for an integer in a sorted array of integers.
+ * <p>
+ * The <em>indexOf</em> operations takes logarithmic time in the worst case.
+ * <p>
+ * For additional documentation, see <a href="https://algs4.cs.princeton.edu/11model">Section 1.1</a> of
+ * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * @author Robert Sedgewick
+ * @author Kevin Wayne
  */
 public class BinarySearch {
 
     /**
      * This class should not be instantiated.
      */
-    private BinarySearch() { }
+    private BinarySearch() {
+    }
 
     /**
      * Returns the index of the specified key in the specified array.
      *
-     * @param  a the array of integers, must be sorted in ascending order
-     * @param  key the search key
+     * @param a   the array of integers, must be sorted in ascending order
+     * @param key the search key
      * @return index of key in array {@code a} if present; {@code -1} otherwise
      */
     public static int indexOf(int[] a, int key) {
@@ -59,9 +60,13 @@ public class BinarySearch {
         while (lo <= hi) {
             // Key is in a[lo..hi] or not present.
             int mid = lo + (hi - lo) / 2;
-            if      (key < a[mid]) hi = mid - 1;
-            else if (key > a[mid]) lo = mid + 1;
-            else return mid;
+            if (key < a[mid]) {
+                hi = mid - 1;
+            } else if (key > a[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
         }
         return -1;
     }
@@ -71,8 +76,8 @@ public class BinarySearch {
      * This function is poorly named because it does not give the <em>rank</em>
      * if the array has duplicate keys or if the key is not in the array.
      *
-     * @param  key the search key
-     * @param  a the array of integers, must be sorted in ascending order
+     * @param key the search key
+     * @param a   the array of integers, must be sorted in ascending order
      * @return index of key in array {@code a} if present; {@code -1} otherwise
      * @deprecated Replaced by {@link #indexOf(int[], int)}.
      */
@@ -100,8 +105,9 @@ public class BinarySearch {
         // read integer key from standard input; print if not in allowlist
         while (!StdIn.isEmpty()) {
             int key = StdIn.readInt();
-            if (BinarySearch.indexOf(allowlist, key) == -1)
+            if (BinarySearch.indexOf(allowlist, key) == -1) {
                 StdOut.println(key);
+            }
         }
     }
 }
