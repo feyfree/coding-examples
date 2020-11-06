ac0c707572d18f85ec707d71c1a66818789100e3
leilei <feyfree@163.com>
2020-11-06 18:54:40 +0800

    [REV] add bit operation

---
+++ src/main/java/com/feyfree/basic/operation/AdvancedBitOperation.java
@@ -0,0 +1,48 @@
+package com.feyfree.basic.operation;
+
+/**
+ * 复杂的位运算
+ *
+ * @author leilei
+ */
+public class AdvancedBitOperation {
+    /**
+     * 判断奇偶性质  利用位运算
+     * <p>
+     * 现代编译器在编译阶段实际上 会将判断并优化成位运算
+     *
+     * @param x 输入
+     * @return true 是 odd false 是even
+     */
+    public boolean isOdd(int x) {
+        return (x & 1) == 1;
+    }
+
+    /**
+     * 清零 最低位 的 1
+     *
+     * @param x 输入
+     * @return 清零 最低位 的 1
+     */
+    public int removeLowestOne(int x) {
+        return x & (x - 1);
+    }
+
+    /**
+     * 得到最低位的 1
+     *
+     * @param x 输入
+     * @return 输出
+     */
+    public int getLowestOne(int x) {
+        return x & -x;
+    }
+
+
+    public static void main(String[] args) {
+        AdvancedBitOperation operation = new AdvancedBitOperation();
+        System.out.println(operation.isOdd(1));
+    }
+
+
+}
