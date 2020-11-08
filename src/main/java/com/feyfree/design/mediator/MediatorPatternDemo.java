8f308ff786e4608618a33f0eb53dc06b8773edf8
leilei <feyfree@163.com>
2020-11-08 16:33:02 +0800

    [ADD] add mediator

---
+++ src/main/java/com/feyfree/design/mediator/MediatorPatternDemo.java
@@ -0,0 +1,14 @@
+package com.feyfree.design.mediator;
+
+/**
+ * @author feyfree
+ */
+public class MediatorPatternDemo {
+    public static void main(String[] args) {
+        User robert = new User("Robert");
+        User john = new User("John");
+
+        robert.sendMessage("Hi! John!");
+        john.sendMessage("Hello! Robert!");
+    }
+}
