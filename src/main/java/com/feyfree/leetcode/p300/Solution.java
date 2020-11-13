cddcb290436c1fcad3050e45e4a00bb80b6c8aa9
leilei <feyfree@163.com>
2020-11-13 17:23:04 +0800

    [ADD] add leetcode 300 solution

---
+++ src/main/java/com/feyfree/leetcode/p300/Solution.java
@@ -0,0 +1,32 @@
+package com.feyfree.leetcode.p300;
+
+import java.util.Arrays;
+
+/**
+ * p300 最长上升子序列(不需要连续)
+ *
+ * @author feyfree
+ */
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[] dp = new int[n];
+        // 初始化dp 实际上都是 1
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, dp[i]);
+        }
+        return result;
+    }
+}
