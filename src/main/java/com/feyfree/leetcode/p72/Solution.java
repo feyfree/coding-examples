54752c8a09a304acc6727c0f149bebfe4f762352
leilei <feyfree@163.com>
2020-11-17 09:18:15 +0800

    [ADD] add leetcode 72 solution template

---
+++ src/main/java/com/feyfree/leetcode/p72/Solution.java
@@ -0,0 +1,24 @@
+package com.feyfree.leetcode.p72;
+
+/**
+ * 给你两个单词 word1 和 word2，
+ * 请你计算出将 word1 转换成 word2
+ * 所使用的最少操作数 。
+ * <p>
+ * 你可以对一个单词进行如下三种操作：
+ * <p>
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/edit-distance
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author leilei
+ */
+public class Solution {
+    public int minDistance(String word1, String word2) {
+        return 0;
+    }
+}
