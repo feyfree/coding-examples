6a76f55891a17b9228ff34be613b637bc3009cce
leilei <feyfree@163.com>
2020-11-18 09:25:48 +0800

    [ADD] add leetcode 72 solution

--- src/main/java/com/feyfree/leetcode/p72/Solution.java
+++ src/main/java/com/feyfree/leetcode/p72/Solution.java
@@ -19,6 +19,34 @@ package com.feyfree.leetcode.p72;
  */
 public class Solution {
     public int minDistance(String word1, String word2) {
-        return 0;
+        if (word1.isEmpty() || word2.isEmpty()) {
+            return Math.max(word1.length(), word2.length());
+        }
+        int n = word1.length();
+        int m = word2.length();
+        // dp[][] 里面要保存多一位
+        int[][] dp = new int[n + 1][m + 1];
+        // 初始化
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.minDistance("", "12");
     }
 }
