92faa23a9b23c11bcdddf6cac794168c0b230f31
leilei <feyfree@163.com>
2020-11-19 17:00:24 +0800

    [ADD] add leetcode uf solution template

---
+++ src/main/java/com/feyfree/leetcode/p200/Solution.java
@@ -0,0 +1,26 @@
+package com.feyfree.leetcode.p200;
+
+/**
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * <p>
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+ * <p>
+ * 此外，你可以假设该网格的四条边均被水包围
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-islands
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author leilei
+ */
+public class Solution {
+    /**
+     * 使用并查集的结构
+     *
+     * @param grid 输入
+     * @return 返回岛屿数目
+     */
+    public int numIslands(char[][] grid) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/feyfree/leetcode/p547/Solution.java
@@ -0,0 +1,12 @@
+package com.feyfree.leetcode.p547;
+
+/**
+ * 547
+ *
+ * @author leilei
+ */
+public class Solution {
+    public int findCircleNum(int[][] M) {
+        return 0;
+    }
+}
