0c75f535fda08efa0e848c659cd6f31ae168db6a
leilei <feyfree@163.com>
2020-11-20 18:02:04 +0800

    [REV] 547 solution add comment

--- src/main/java/com/feyfree/leetcode/p547/Solution.java
+++ src/main/java/com/feyfree/leetcode/p547/Solution.java
@@ -19,6 +19,16 @@ public class Solution {
         return count;
     }
 
+    /**
+     * dfs 搜索路径需要调整
+     * 因为是对角线问题 需要一个跳跃 M[i][j] == 1 的话， 需要跳到 j 行进行搜索
+     * 比如用户 A B C D
+     * M[A][B] == 1 则需要到B 的 acquaintance 里面进行搜索
+     *
+     * @param M       输入
+     * @param visited 访问标记
+     * @param i       需要访问的用户
+     */
     public void dfs(int[][] M, int[] visited, int i) {
         for (int j = 0; j < M.length; j++) {
             if (M[i][j] == 1 && visited[j] == 0) {
