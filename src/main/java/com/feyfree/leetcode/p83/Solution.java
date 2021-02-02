79ff077382a909122c3942bd5cbf88142bc00b4d
leilei <gengll2@yunjiglobal.com>
2021-02-02 23:00:42 +0800

    [ADD] 新增leetcode Solution

---
+++ src/main/java/com/feyfree/leetcode/p83/Solution.java
@@ -0,0 +1,24 @@
+package com.feyfree.leetcode.p83;
+
+import com.feyfree.leetcode.commons.ListNode;
+
+/**
+ * p83
+ *
+ * @author leilei
+ */
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode p = dummy.next;
+        while (p != null && p.next != null) {
+            if (p.val == p.next.val) {
+                p.next = p.next.next;
+            } else {
+                p = p.next;
+            }
+        }
+        return dummy.next;
+    }
+}
