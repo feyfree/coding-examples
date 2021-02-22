ae166743723d60cdec65b8af0738112e1d5ad956
leilei <gengll2@yunjiglobal.com>
2021-02-22 22:45:26 +0800

    [ADD] 新增 leetcode 89 solution template

---
+++ src/main/java/com/feyfree/leetcode/p89/Solution.java
@@ -0,0 +1,13 @@
+package com.feyfree.leetcode.p89;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author leilei
+ */
+class Solution {
+    public List<Integer> grayCode(int n) {
+        return new ArrayList<>();
+    }
+}
