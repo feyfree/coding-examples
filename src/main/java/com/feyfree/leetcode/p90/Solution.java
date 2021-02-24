186d095f7de416e7f8ca5c7234c68627336526ba
leilei <feyfree@163.com>
2021-02-24 19:35:51 +0800

    [REV] 增加leetcode 90 need modify

--- src/main/java/com/feyfree/leetcode/p90/Solution.java
+++ src/main/java/com/feyfree/leetcode/p90/Solution.java
@@ -15,24 +15,30 @@ public class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         int n = nums.length;
+        boolean[] used = new boolean[n];
         for (int i = 0; i < n; i++) {
-            dfs(n, i, 0, new ArrayList<>(), result, nums);
+            dfs(n, i, 0, new ArrayList<>(), result, nums, used);
         }
         return result;
     }
 
-    private void dfs(int n, int k, int d, List<Integer> current, List<List<Integer>> result, int[] nums) {
+    private void dfs(int n, int k, int d, List<Integer> current, List<List<Integer>> result, int[] nums, boolean[] used) {
         if (current.size() == k) {
             result.add(new ArrayList<>(current));
             return;
         }
         for (int i = d; i < n; i++) {
-            if (i > d && nums[i] == nums[i - 1]) {
+            if (used[i]) {
+                continue;
+            }
+            if (i > d && nums[i] == nums[i - 1] && !used[i]) {
                 continue;
             }
             current.add(nums[i]);
-            dfs(n, k, i + 1, current, result, nums);
+            used[i] = true;
+            dfs(n, k, i + 1, current, result, nums, used);
             current.remove(current.size() - 1);
+            used[i] = false;
         }
     }
 }
