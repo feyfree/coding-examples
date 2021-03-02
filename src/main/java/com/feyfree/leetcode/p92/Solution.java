44a7b3c91244b6eebf7952939bf56157490f1ede
leilei <feyfree@163.com>
2021-03-02 18:05:56 +0800

    [REV] 增加简单解法 92 题解

--- src/main/java/com/feyfree/leetcode/p92/Solution.java
+++ src/main/java/com/feyfree/leetcode/p92/Solution.java
@@ -9,23 +9,23 @@ import com.feyfree.leetcode.commons.ListNode;
  */
 public class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        int start = 0;
-        ListNode cur = head;
-        while (start < left) {
-            cur = cur.next;
-            start++;
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode p = dummy;
+        for (int i = 0; i < left - 1; i++) {
+            p = p.next;
         }
-        ListNode stop = cur;
-        ListNode pre = null;
-        while (start < right) {
-            // 表示当前节点
-            ListNode temp = cur;
-            cur = cur.next;
-            temp.next = pre;
-            pre = temp;
-            start++;
+        ListNode pre = p;
+        ListNode cur = p.next;
+        ListNode tail = cur;
+        for (int i = left; i <= right; i++) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
         }
-        stop.next = pre;
-        return head;
+        p.next = pre;
+        tail.next = cur;
+        return dummy.next;
     }
 }
