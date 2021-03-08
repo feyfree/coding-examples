c2e356b14cd9fa5033c8e714a632a45d7f46190b
leilei <feyfree@163.com>
2021-03-08 18:45:50 +0800

    [REV] leetcode 95

---
+++ src/main/java/com/feyfree/leetcode/p0095/Solution.java
@@ -0,0 +1,39 @@
+package com.feyfree.leetcode.p0095;
+
+import com.feyfree.leetcode.commons.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hint https://zxi.mytechroad.com/blog/uncategorized/leetcode-95-unique-binary-search-trees-ii/
+ *
+ * @author leilei
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int l, int r) {
+        List<TreeNode> ans = new ArrayList<>();
+        if (l > r) {
+            ans.add(null);
+            return ans;
+        }
+        for (int i = l; i <= r; ++i) {
+            for (TreeNode left : generateTrees(l, i - 1)) {
+                for (TreeNode right : generateTrees(i + 1, r)) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    ans.add(root);
+                }
+            }
+        }
+        return ans;
+    }
+}
