dcb910dd7c71b67c80abd4f3b145151d01d124bd
Brisk <feyfree@163.com>
2021-03-17 23:39:20 +0800

    add generics test

---
+++ src/main/java/com/feyfree/jvm/generics/GenericTypes.java
@@ -0,0 +1,21 @@
+package com.feyfree.jvm.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericTypes {
+//    public static String method(List<String> list) {
+//        System.out.println("invoke method(List<String> list)");
+//        return "";
+//    }
+//
+//    public static int method(List<Integer> list) {
+//        System.out.println("invoke method(List<Integer> list)");
+//        return 1;
+//    }
+//
+//    public static void main(String[] args) {
+//        method(new ArrayList<String>());
+//        method(new ArrayList<Integer>());
+//    }
+}
