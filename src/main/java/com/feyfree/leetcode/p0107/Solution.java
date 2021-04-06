9100a916dea6887b620b7ab4c2361e28e7cf9d5d
leilei <feyfree@163.com>
2021-04-06 14:35:30 +0800

    [REV] add leetcode 107 solution

--- src/main/java/com/feyfree/leetcode/p0107/Solution.java
+++ src/main/java/com/feyfree/leetcode/p0107/Solution.java
@@ -3,10 +3,40 @@ package com.feyfree.leetcode.p0107;
 import com.feyfree.leetcode.commons.TreeNode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+/**
+ * leetcode 102 题 最后添加reverse即可
+ *
+ * @author leilei
+ */
 class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        return new ArrayList<>();
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        // 当前需要访问的node 列表
+        List<TreeNode> curr = new ArrayList<>();
+        // 下一次需要访问的 node 列表
+        List<TreeNode> next = new ArrayList<>();
+        curr.add(root);
+        while (!curr.isEmpty()) {
+            result.add(new ArrayList<>());
+            for (TreeNode node : curr) {
+                result.get(result.size() - 1).add(node.val);
+                if (node.left != null) {
+                    next.add(node.left);
+                }
+                if (node.right != null) {
+                    next.add(node.right);
+                }
+            }
+            curr = new ArrayList<>(next);
+            next.clear();
+        }
+        Collections.reverse(result);
+        return result;
     }
 }
