c35138befe2a4f2facdd8b49ee975e0907e2409e
leilei <feyfree@163.com>
2021-04-07 11:40:33 +0800

    [REV] add leetcode 108 solution

--- src/main/java/com/feyfree/leetcode/p0108/Solution.java
+++ src/main/java/com/feyfree/leetcode/p0108/Solution.java
@@ -1,4 +1,39 @@
 package com.feyfree.leetcode.p0108;
 
-public class Solution {
+import com.feyfree.leetcode.commons.TreeNode;
+
+/**
+ * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+ * <p>
+ * 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author leilei
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildBST(nums, 0, nums.length - 1);
+    }
+
+    /**
+     * 构建平衡二叉搜索树
+     *
+     * @param nums 有序数组
+     * @param l    左边界
+     * @param r    右边界
+     * @return 节点
+     */
+    private TreeNode buildBST(int[] nums, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+        int m = l + (r - l) / 2;
+        TreeNode root = new TreeNode(nums[m]);
+        root.left = buildBST(nums, l, m - 1);
+        root.right = buildBST(nums, m + 1, r);
+        return root;
+    }
 }
