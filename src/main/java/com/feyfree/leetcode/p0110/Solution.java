09f0aac6fc9e0582cd2ffcdaeede702faf0c0f79
Brisk <feyfree@163.com>
2021-04-07 21:54:19 +0800

    [ADD] add new solution template

---
+++ src/main/java/com/feyfree/leetcode/p0110/Solution.java
@@ -0,0 +1,14 @@
+package com.feyfree.leetcode.p0110;
+
+import com.feyfree.leetcode.commons.TreeNode;
+
+/**
+ * 检查是否是平衡二叉树
+ *
+ * @author leilei
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return false;
+    }
+}
