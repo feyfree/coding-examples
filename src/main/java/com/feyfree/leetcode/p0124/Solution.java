20469d2ae9592914be368b5341ca545a00395937
leilei <feyfree@163.com>
2021-04-27 11:01:35 +0800

    [REV] add leetcode 124 template

---
+++ src/main/java/com/feyfree/leetcode/p0124/Solution.java
@@ -0,0 +1,27 @@
+package com.feyfree.leetcode.p0124;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        return 0;
+    }
+}
