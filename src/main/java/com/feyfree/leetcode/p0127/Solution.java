61778dba365c9decd117720e5a4718eae81afd4d
Brisk <feyfree@163.com>
2021-05-06 21:37:31 +0800

    [ADD] add new solution template

---
+++ src/main/java/com/feyfree/leetcode/p0127/Solution.java
@@ -0,0 +1,16 @@
+package com.feyfree.leetcode.p0127;
+
+import java.util.List;
+
+
+/**
+ * 126. 单词接龙
+ * https://leetcode-cn.com/problems/word-ladder/
+ *
+ * @author leilei
+ */
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        return 0;
+    }
+}
