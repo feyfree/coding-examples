bacc4ea9f3de75314edc16612f9dcd84fb8777d8
leilei <feyfree@163.com>
2021-05-07 15:56:31 +0800

    [REV] add leetcode solution

--- src/main/java/com/feyfree/leetcode/p0126/Solution.java
+++ src/main/java/com/feyfree/leetcode/p0126/Solution.java
@@ -1,15 +1,91 @@
 package com.feyfree.leetcode.p0126;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 126. 单词接龙 II
  *
  * @author leilei
  */
-class Solution {
+public class Solution {
+
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        return new ArrayList<>();
+        List<List<String>> res = new ArrayList<>();
+        Set<String> dict = new HashSet<>(wordList);
+        if (!dict.contains(endWord)) {
+            return res;
+        }
+        dict.remove(beginWord);
+        // 第 1 步：广度优先遍历建图
+        // 记录扩展出的单词是在第几次扩展的时候得到的，key：单词，value：在广度优先遍历的第几层
+        Map<String, Integer> steps = new HashMap<>();
+        steps.put(beginWord, 0);
+        // 记录了单词是从哪些单词扩展而来，key：单词，value：单词列表，这些单词可以变换到 key ，它们是一对多关系
+        Map<String, List<String>> parents = new HashMap<>();
+        int step = 1;
+        boolean found = false;
+        int l = beginWord.length();
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String currWord = queue.poll();
+                char[] chars = currWord.toCharArray();
+                // 将每一位替换成 26 个小写英文字母
+                for (int j = 0; j < l; j++) {
+                    char origin = chars[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        chars[j] = c;
+                        String nextWord = String.valueOf(chars);
+                        if (steps.containsKey(nextWord) && step == steps.get(nextWord)) {
+                            parents.get(nextWord).add(currWord);
+                        }
+                        if (!dict.contains(nextWord)) {
+                            continue;
+                        }
+                        // 如果从一个单词扩展出来的单词以前遍历过，距离一定更远，为了避免搜索到已经遍历到，且距离更远的单词，需要将它从 dict 中删除
+                        dict.remove(nextWord);
+                        // 这一层扩展出的单词进入队列
+                        queue.offer(nextWord);
+
+                        // 记录 nextWord 从 currWord 而来
+                        parents.putIfAbsent(nextWord, new ArrayList<>());
+                        parents.get(nextWord).add(currWord);
+                        // 记录 nextWord 的 step
+                        steps.put(nextWord, step);
+                        if (nextWord.equals(endWord)) {
+                            found = true;
+                        }
+                    }
+                    chars[j] = origin;
+                }
+            }
+            step++;
+            if (found) {
+                break;
+            }
+        }
+
+        // 第 2 步：深度优先遍历找到所有解，从 endWord 恢复到 beginWord ，所以每次尝试操作 path 列表的头部
+        if (found) {
+            Deque<String> path = new ArrayDeque<>();
+            path.add(endWord);
+            dfs(parents, path, beginWord, endWord, res);
+        }
+        return res;
+    }
+
+    public void dfs(Map<String, List<String>> parents, Deque<String> path, String beginWord, String cur, List<List<String>> res) {
+        if (cur.equals(beginWord)) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (String item : parents.get(cur)) {
+            path.addFirst(item);
+            dfs(parents, path, beginWord, item, res);
+            path.removeFirst();
+        }
     }
 }
+
