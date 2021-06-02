1ee349c797932d711e948effc743b82b147d3aca
leilei <feyfree@163.com>
2021-06-02 14:44:38 +0800

    [REV] add leetcode 154 solution

--- src/main/java/com/feyfree/leetcode/p0154/Solution.java
+++ src/main/java/com/feyfree/leetcode/p0154/Solution.java
@@ -15,11 +15,33 @@ public class Solution {
             if (nums[mid] < nums[high]) {
                 high = mid;
             } else if (nums[mid] == nums[high]) {
-                // todo
+                boolean increase = increaseSequence(nums, mid, high);
+                if (increase) {
+                    high = mid;
+                } else {
+                    low = mid + 1;
+                }
             } else {
                 low = mid + 1;
             }
         }
         return nums[low];
+    }
+
+    /**
+     * 是否是递增序列
+     *
+     * @param nums  数组
+     * @param start 起点
+     * @param end   终点
+     * @return true 递增 false 不是递增
+     */
+    private boolean increaseSequence(int[] nums, int start, int end) {
+        for (int i = start + 1; i <= end; i++) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
