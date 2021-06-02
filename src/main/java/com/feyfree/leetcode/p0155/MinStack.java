8b9ff2ceaef2f5be078c251c27e0d2641f71d208
leilei <feyfree@163.com>
2021-06-02 14:51:23 +0800

    [REV] add leetcode 155 template

---
+++ src/main/java/com/feyfree/leetcode/p0155/MinStack.java
@@ -0,0 +1,43 @@
+package com.feyfree.leetcode.p0155;
+
+/**
+ * 155. 最小栈
+ * https://leetcode-cn.com/problems/min-stack/
+ *
+ * @author leilei
+ */
+class MinStack {
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        return 0;
+
+    }
+
+    public int getMin() {
+        return 0;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
