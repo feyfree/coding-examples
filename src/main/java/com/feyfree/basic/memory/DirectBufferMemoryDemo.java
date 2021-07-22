a0726dc8aadde69c3b6319f9165cca669c6ec164
leilei <feyfree@163.com>
2021-07-22 18:00:51 +0800

    [REV] add gc demo

--- src/main/java/com/feyfree/basic/memory/DirectBufferMemoryDemo.java
+++ src/main/java/com/feyfree/basic/memory/DirectBufferMemoryDemo.java
@@ -2,14 +2,20 @@ package com.feyfree.basic.memory;
 
 import sun.misc.VM;
 
+import java.nio.ByteBuffer;
+
 /**
  * 堆外内存
  *
  * @author leilei
  */
-public class DirectMemoryErrorDemo {
+public class DirectBufferMemoryDemo {
     public static void main(String[] args) {
         String memory = (VM.maxDirectMemory() / (double) 1024 / 1024) + "MB";
         System.out.println("配置的最大的堆外内存:" + memory);
+
+        while (true) {
+            ByteBuffer bb = ByteBuffer.allocateDirect(100 * 1024 * 1024);
+        }
     }
 }
