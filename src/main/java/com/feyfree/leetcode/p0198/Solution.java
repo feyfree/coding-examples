ba9871c9891ccd41ba2e13553504496222e08602
leilei <feyfree@163.com>
2021-08-02 11:45:31 +0800

    [REV] add new solution

--- src/main/java/com/feyfree/leetcode/p0198/Solution.java
+++ src/main/java/com/feyfree/leetcode/p0198/Solution.java
@@ -3,11 +3,26 @@ package com.feyfree.leetcode.p0198;
 /**
  * 198. 打家劫舍
  * https://leetcode-cn.com/problems/house-robber/
+ * 滚动数组
  *
  * @author leilei
  */
 public class Solution {
     public int rob(int[] nums) {
-        return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        } else {
+            int a = nums[0];
+            int b = nums[1];
+            b = Math.max(a, b);
+            for (int i = 2; i < nums.length; i++) {
+                int temp = a;
+                a = b;
+                b = Math.max(b, temp + nums[i]);
+            }
+            return b;
+        }
     }
 }
