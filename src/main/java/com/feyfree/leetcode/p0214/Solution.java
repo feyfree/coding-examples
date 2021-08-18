49454343b0fc6ef576eae31cf77c1bd5c1c492df
feyfree <feyfree@163.com>
2021-08-18 16:26:57 +0800

    [ADD] add 214 solution template

---
+++ src/main/java/com/feyfree/leetcode/p0214/Solution.java
@@ -0,0 +1,26 @@
+package com.feyfree.leetcode.p0214;
+
+/**
+ * 214. 最短回文串
+ * 给定一个字符串 s，你可以通过在字符串前面添加字符将其转换为回文串。找到并返回可以用这种方式转换的最短回文串。
+ * <p>
+ * s = 回文 + T
+ * Tr + 回文 + T
+ *
+ * @author leilei
+ */
+public class Solution {
+    public String shortestPalindrome(String s) {
+        return null;
+    }
+
+    /**
+     * 找到一个字符串 最长的suffix palindrome
+     *
+     * @param a 输入
+     * @return 起点索引
+     */
+    private int longestPrefixPalindrome(String a) {
+        return 0;
+    }
+}
