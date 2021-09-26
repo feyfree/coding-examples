05dbd080cdd31a25437c52456db703157afc24a6
leilei <feyfree@163.com>
2021-09-26 18:50:45 +0800

    [REV] add leetcode solution template

---
+++ src/main/java/com/feyfree/leetcode/p0241/Solution.java
@@ -0,0 +1,16 @@
+package com.feyfree.leetcode.p0241;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/different-ways-to-add-parentheses/
+ * 241. 为运算表达式设计优先级
+ *
+ * @author leilei
+ */
+public class Solution {
+    public List<Integer> diffWaysToCompute(String expression) {
+        return new ArrayList<>();
+    }
+}
