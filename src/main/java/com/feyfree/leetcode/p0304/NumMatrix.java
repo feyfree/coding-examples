26ca5882bc3707f0f030705221ad7326cf3a7358
Brisk <feyfree@163.com>
2021-10-26 22:40:31 +0800

    [ADD] add leetcode solution

--- src/main/java/com/feyfree/leetcode/p0304/NumMatrix.java
+++ src/main/java/com/feyfree/leetcode/p0304/NumMatrix.java
@@ -6,37 +6,24 @@ package com.feyfree.leetcode.p0304;
  *
  * @author leilei
  */
-public class NumMatrix {
-
-    private int[][] sums;
+class NumMatrix {
+    int[][] sums;
 
     public NumMatrix(int[][] matrix) {
-        this.sums = new int[matrix.length][matrix[0].length];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (j > 0) {
-                    sums[i][j] = sums[i][j - 1] + matrix[i][j];
-                } else {
-                    sums[i][j] = matrix[i][j];
+        int m = matrix.length;
+        if (m > 0) {
+            int n = matrix[0].length;
+            sums = new int[m + 1][n + 1];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    sums[i + 1][j + 1] = sums[i][j + 1] + sums[i + 1][j] - sums[i][j] + matrix[i][j];
                 }
             }
         }
-
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int ax = Math.min(row1, row2);
-        int ay = Math.min(col1, col2);
-
-        int bx = ax;
-        int by = Math.max(col1, col2);
-
-        int cx = Math.max(row1, row2);
-        int cy = Math.min(col1, col2);
-
-        int dx = cx;
-        int dy = Math.max(col1, col2);
-
-        return sums[dx][dy] + sums[ax][ay] - sums[bx][by] - sums[cx][cy];
+        return sums[row2 + 1][col2 + 1] - sums[row1][col2 + 1] - sums[row2 + 1][col1] + sums[row1][col1];
     }
 }
+
