1a37a04345a3c695bd814508f1cbd7b376b596b7
leilei <feyfree@163.com>
2021-12-01 18:13:56 +0800

    [REV] add new solution template

---
+++ src/main/java/com/feyfree/leetcode/p0368/Solution.java
@@ -0,0 +1,17 @@
+package com.feyfree.leetcode.p0368;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 368. 最大整除子集
+ * https://leetcode-cn.com/problems/largest-divisible-subset/
+ *
+ * @author leilei
+ */
+public class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        return null;
+    }
+}
