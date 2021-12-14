60778c9eb46c1d7b7372150e598276bb235f1a03
leilei <feyfree@163.com>
2021-12-14 09:51:56 +0800

    [REV] leetcode 394 modified

--- src/main/java/com/feyfree/leetcode/p0394/Solution.java
+++ src/main/java/com/feyfree/leetcode/p0394/Solution.java
@@ -47,7 +47,7 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        solution.decodeString("3[a]2[bc]");
+        System.out.println(solution.decodeString("3[a]2[bc]"));
     }
 }
 
