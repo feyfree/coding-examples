fa93c1bb928ea35b134115153f77e7a0db82a877
leilei <feyfree@163.com>
2021-12-16 17:57:24 +0800

    [REV] add solution template

---
+++ src/main/java/com/feyfree/leetcode/p0386/Solution.java
@@ -0,0 +1,16 @@
+package com.feyfree.leetcode.p0386;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/lexicographical-numbers/
+ * 386. 字典序排数
+ *
+ * @author leilei
+ */
+public class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        return new ArrayList<>(n);
+    }
+}
