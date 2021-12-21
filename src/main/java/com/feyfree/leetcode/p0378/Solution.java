9d8233c83b19757aede64be68a4327b4dfa340df
Brisk <feyfree@163.com>
2021-12-21 23:03:50 +0800

    [ADD] add leetcode solution

---
+++ src/main/java/com/feyfree/leetcode/p0378/Solution.java
@@ -0,0 +1,12 @@
+package com.feyfree.leetcode.p0378;
+
+/**
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-sorted-matrix/
+ *
+ * @author leilei
+ */
+public class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        return 0;
+    }
+}
