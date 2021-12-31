db91d8c8978af49a8583d3d14295d4d3add58a5c
leilei <feyfree@163.com>
2021-12-31 11:05:44 +0800

    [REV] add solution template

---
+++ src/main/java/com/feyfree/leetcode/p0335/Solution.java
@@ -0,0 +1,14 @@
+package com.feyfree.leetcode.p0335;
+
+/**
+ * https://leetcode-cn.com/problems/self-crossing/
+ * <p>
+ * 335. 路径交叉
+ *
+ * @author leilei
+ */
+public class Solution {
+    public boolean isSelfCrossing(int[] distance) {
+        return false;
+    }
+}
