005e710b1956e38f8392b526780e5a1e05caade2
leilei <feyfree@163.com>
2022-01-07 09:20:11 +0800

    [REV] add solution template

---
+++ src/main/java/com/feyfree/leetcode/p0402/Solution.java
@@ -0,0 +1,13 @@
+package com.feyfree.leetcode.p0402;
+
+/**
+ * 402. 移掉 K 位数字
+ * https://leetcode-cn.com/problems/remove-k-digits/
+ *
+ * @author leilei
+ */
+public class Solution {
+    public String removeKdigits(String num, int k) {
+        return "";
+    }
+}
