dd23070a243460133fa0f6003a689988bfdf49f8
leilei <feyfree@163.com>
2022-01-10 18:00:03 +0800

    [REV] add solution template

---
+++ src/main/java/com/feyfree/leetcode/p0407/Solution.java
@@ -0,0 +1,12 @@
+package com.feyfree.leetcode.p0407;
+
+/**
+ * https://leetcode-cn.com/problems/trapping-rain-water-ii/
+ *
+ * @author leilei
+ */
+public class Solution {
+    public int trapRainWater(int[][] heightMap) {
+        return 0;
+    }
+}
