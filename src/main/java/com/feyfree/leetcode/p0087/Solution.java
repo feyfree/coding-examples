6c8b757f308cea4fd6563846733a19fdaf9a472a
leilei <feyfree@163.com>
2022-01-24 17:34:17 +0800

    [REV] solution modified

--- src/main/java/com/feyfree/leetcode/p0087/Solution.java
+++ src/main/java/com/feyfree/leetcode/p0087/Solution.java
@@ -1,29 +1,49 @@
 package com.feyfree.leetcode.p0087;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * p87 扰乱字符串
  * <p>
  * hint by:
  * https://zxi.mytechroad.com/blog/searching/leetcode-87-scramble-string/
+ * ----------
+ * 20220124测试用例更新, 需要增加一个map用于保存临时状态
+ * key: s1 + s2
+ * val: bool
+ * <p>
+ * updated
  *
  * @author leilei
  */
 class Solution {
+
+    private Map<String, Boolean> map = new HashMap<>();
+
     public boolean isScramble(String s1, String s2) {
         int l = s1.length();
+        String key = s1 + s2;
+        if (map.containsKey(key)) {
+            return map.get(key);
+        }
         if (s1.equals(s2)) {
+            map.put(key, true);
             return true;
         }
         if (!sameFrequency(s1, s2)) {
+            map.put(key, false);
             return false;
         }
         for (int i = 1; i < l; i++) {
             boolean valid = isScramble(s1.substring(0, i), s2.substring(0, i)) && isScramble(s1.substring(i), s2.substring(i))
                     || isScramble(s1.substring(0, i), s2.substring(l - i, l)) && isScramble(s1.substring(i), s2.substring(0, l - i));
             if (valid) {
+                map.put(key, true);
                 return true;
             }
         }
+        map.put(key, false);
         return false;
     }
 
