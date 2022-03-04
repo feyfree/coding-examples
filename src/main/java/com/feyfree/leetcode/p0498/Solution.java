67a3049855823a305d1673aae524eb3e398f52c0
leilei <feyfree@163.com>
2022-03-04 16:30:31 +0800

    [REV] add solution template

---
+++ src/main/java/com/feyfree/leetcode/p0498/Solution.java
@@ -0,0 +1,12 @@
+package com.feyfree.leetcode.p0498;
+
+/**
+ * https://leetcode-cn.com/problems/diagonal-traverse/
+ *
+ * @author leilei
+ */
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        return null;
+    }
+}
