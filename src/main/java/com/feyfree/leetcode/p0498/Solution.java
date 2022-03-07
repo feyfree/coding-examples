f552d900cfc6764e4b7fe366e78ff7c222ccf426
leilei <feyfree@163.com>
2022-03-07 17:52:54 +0800

    [REV] add new solution - need modify

--- src/main/java/com/feyfree/leetcode/p0498/Solution.java
+++ src/main/java/com/feyfree/leetcode/p0498/Solution.java
@@ -7,6 +7,50 @@ package com.feyfree.leetcode.p0498;
  */
 class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
-        return null;
+        int rows = mat.length;
+        int cols = mat[0].length;
+        int[] result = new int[rows * cols];
+
+        int row = 0;
+        int col = 0;
+        int minRow = 0;
+        int minCol = 0;
+        // 初始是 up
+        int dir = 1;
+        int count = 0;
+        while (count < rows * cols) {
+            result[count] = mat[row][col];
+            count++;
+            if (dir == 1) {
+                if (row == minRow) {
+                    if (col + 1 < cols) {
+                        col = col + 1;
+                    } else {
+                        row = minRow + 1;
+                        col = cols - 1;
+                        dir = 0;
+                        minRow = minRow + 1;
+                    }
+                } else {
+                    row--;
+                    col++;
+                }
+            } else {
+                if (col == minCol) {
+                    if (row + 1 < rows) {
+                        row = row + 1;
+                    } else {
+                        col = minCol + 1;
+                        row = rows - 1;
+                        minCol = minCol + 1;
+                        dir = 1;
+                    }
+                } else {
+                    row++;
+                    col--;
+                }
+            }
+        }
+        return result;
     }
 }
