7ecdd944ef8dc319824be88401e168662a4ee21e
leilei <feyfree@163.com>
2022-03-15 17:50:08 +0800

    [REV] add solution template

---
+++ src/main/java/com/feyfree/leetcode/p0476/Solution.java
@@ -0,0 +1,18 @@
+package com.feyfree.leetcode.p0476;
+
+/**
+ * https://leetcode-cn.com/problems/number-complement/
+ *
+ * @author leilei
+ */
+public class Solution {
+    public int findComplement(int num) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(~1);
+        System.out.println(Integer.toBinaryString(-2));
+        System.out.println(Integer.toBinaryString(-5));
+    }
+}
