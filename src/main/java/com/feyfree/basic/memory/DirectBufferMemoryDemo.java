0303419e602edbbd581716014308d8ed15cd7ec9
feyfree <feyfree@163.com>
2022-03-26 20:22:26 +0800

    [REV] update lombok, remove some code not compatible with new computer settings

--- src/main/java/com/feyfree/basic/memory/DirectBufferMemoryDemo.java
+++ src/main/java/com/feyfree/basic/memory/DirectBufferMemoryDemo.java
@@ -1,21 +1,22 @@
-package com.feyfree.basic.memory;
-
-import sun.misc.VM;
-
-import java.nio.ByteBuffer;
-
-/**
- * 堆外内存
- *
- * @author leilei
- */
-public class DirectBufferMemoryDemo {
-    public static void main(String[] args) {
-        String memory = (VM.maxDirectMemory() / (double) 1024 / 1024) + "MB";
-        System.out.println("配置的最大的堆外内存:" + memory);
-
-        while (true) {
-            ByteBuffer bb = ByteBuffer.allocateDirect(100 * 1024 * 1024);
-        }
-    }
-}
+//package com.feyfree.basic.memory;
+//
+// m1 azul jdk 会有问题
+//import sun.misc.VM;
+//
+//import java.nio.ByteBuffer;
+//
+///**
+// * 堆外内存
+// *
+// * @author leilei
+// */
+//public class DirectBufferMemoryDemo {
+//    public static void main(String[] args) {
+//        String memory = (VM.maxDirectMemory() / (double) 1024 / 1024) + "MB";
+//        System.out.println("配置的最大的堆外内存:" + memory);
+//
+//        while (true) {
+//            ByteBuffer bb = ByteBuffer.allocateDirect(100 * 1024 * 1024);
+//        }
+//    }
+//}
--- src/main/java/com/feyfree/jvm/asm/ClassPrinter.java
+++ src/main/java/com/feyfree/jvm/asm/ClassPrinter.java
@@ -1,73 +1,73 @@
-package com.feyfree.jvm.asm;
-
-import jdk.internal.org.objectweb.asm.*;
-
-import java.io.IOException;
-
-import static jdk.internal.org.objectweb.asm.Opcodes.ASM4;
-
-/**
- * @author leilei
- */
-public class ClassPrinter extends ClassVisitor {
-
-    ClassPrinter() {
-        super(ASM4);
-    }
-
-    @Override
-    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-        System.out.println(name + " extends " + superName + " {");
-    }
-
-    @Override
-    public void visitSource(String source, String debug) {
-    }
-
-    @Override
-    public void visitOuterClass(String owner, String name, String desc) {
-    }
-
-    @Override
-    public AnnotationVisitor visitAnnotation(String desc,
-                                             boolean visible) {
-        return null;
-    }
-
-    @Override
-    public void visitAttribute(Attribute attr) {
-    }
-
-    @Override
-    public void visitInnerClass(String name, String outerName,
-                                String innerName, int access) {
-    }
-
-    @Override
-    public FieldVisitor visitField(int access, String name, String desc,
-                                   String signature, Object value) {
-        System.out.println(" " + desc + " " + name);
-        return null;
-    }
-
-    @Override
-    public MethodVisitor visitMethod(int access, String name,
-                                     String desc, String signature, String[] exceptions) {
-        System.out.println(" " + name + desc);
-        return null;
-    }
-
-    @Override
-    public void visitEnd() {
-        System.out.println("}");
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        ClassPrinter classPrinter = new ClassPrinter();
-        ClassReader classReader = new ClassReader("java.lang.Runnable");
-        ClassReader classReader1 = new ClassReader(ClassPrinter.class.getClassLoader().getResourceAsStream("com/feyfree/algs4/Accumulator.class"));
-        classReader.accept(classPrinter, 0);
-        classReader1.accept(classPrinter, 0);
-    }
-}
+//package com.feyfree.jvm.asm;
+//
+//import jdk.internal.org.objectweb.asm.*;
+//
+//import java.io.IOException;
+//
+//import static jdk.internal.org.objectweb.asm.Opcodes.ASM4;
+//
+///**
+// * @author leilei
+// */
+//public class ClassPrinter extends ClassVisitor {
+//
+//    ClassPrinter() {
+//        super(ASM4);
+//    }
+//
+//    @Override
+//    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+//        System.out.println(name + " extends " + superName + " {");
+//    }
+//
+//    @Override
+//    public void visitSource(String source, String debug) {
+//    }
+//
+//    @Override
+//    public void visitOuterClass(String owner, String name, String desc) {
+//    }
+//
+//    @Override
+//    public AnnotationVisitor visitAnnotation(String desc,
+//                                             boolean visible) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void visitAttribute(Attribute attr) {
+//    }
+//
+//    @Override
+//    public void visitInnerClass(String name, String outerName,
+//                                String innerName, int access) {
+//    }
+//
+//    @Override
+//    public FieldVisitor visitField(int access, String name, String desc,
+//                                   String signature, Object value) {
+//        System.out.println(" " + desc + " " + name);
+//        return null;
+//    }
+//
+//    @Override
+//    public MethodVisitor visitMethod(int access, String name,
+//                                     String desc, String signature, String[] exceptions) {
+//        System.out.println(" " + name + desc);
+//        return null;
+//    }
+//
+//    @Override
+//    public void visitEnd() {
+//        System.out.println("}");
+//    }
+//
+//
+//    public static void main(String[] args) throws IOException {
+//        ClassPrinter classPrinter = new ClassPrinter();
+//        ClassReader classReader = new ClassReader("java.lang.Runnable");
+//        ClassReader classReader1 = new ClassReader(ClassPrinter.class.getClassLoader().getResourceAsStream("com/feyfree/algs4/Accumulator.class"));
+//        classReader.accept(classPrinter, 0);
+//        classReader1.accept(classPrinter, 0);
+//    }
+//}
