ed8f952bc2621da6e8dee521fe2c760ec62fd816
feyfree <feyfree@163.com>
2022-05-10 18:20:51 +0800

    add a demo

---
+++ src/main/java/com/feyfree/jvm/runtime/RuntimeDemo.java
@@ -0,0 +1,7 @@
+package com.feyfree.jvm.runtime;
+
+public class RuntimeDemo {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
