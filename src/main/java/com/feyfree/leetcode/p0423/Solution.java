f741c25bfc8605599c7901eed53d2fa362c86278
feyfree <feyfree@163.com>
2022-06-27 14:15:12 +0800

    [REV] leetcode 423 solution

--- src/main/java/com/feyfree/leetcode/p0423/Solution.java
+++ src/main/java/com/feyfree/leetcode/p0423/Solution.java
@@ -1,8 +1,6 @@
 package com.feyfree.leetcode.p0423;
 
-import com.alibaba.fastjson.JSONObject;
-
-import java.util.*;
+import java.util.Arrays;
 
 /**
  * https://leetcode.cn/problems/reconstruct-original-digits-from-english/
@@ -11,58 +9,38 @@ import java.util.*;
  * @author leilei
  */
 class Solution {
-
-    char[] chars = new char[]{'e', 'g', 'f', 'i', 'h', 'o', 'n', 's', 'r', 'u', 't', 'w', 'v', 'x', 'z'};
-    String[] numbers = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    private final String[] ss = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
     /**
-     * 匹配
+     * 因为 zero 里面有 z, eight 里面有 g
      */
-    Map<String, Integer> values = new HashMap<>();
-
-    Map<Character, List<String>> cMap = new HashMap<>();
-
-
-    private void init() {
-        for (int i = 0; i < numbers.length; i++) {
-            values.put(numbers[i], i);
-            for (int j = 0; j < numbers[i].length(); j++) {
-                char c = numbers[i].charAt(j);
-                if (cMap.containsKey(c)) {
-                    cMap.get(c).add(numbers[i]);
-                } else {
-                    List<String> list = new ArrayList<>();
-                    list.add(numbers[i]);
-                    cMap.put(c, list);
-                }
-            }
-        }
-    }
-
+    private final int[] priority = new int[]{0, 8, 6, 3, 2, 7, 5, 9, 4, 1};
 
     public String originalDigits(String s) {
-        char[] chars = s.toCharArray();
-        int[] nums = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            nums[s.charAt(i) - 'a']++;
+        int n = s.length();
+        int[] counts = new int[26];
+        // step1: mark the number of each char
+        for (int i = 0; i < n; i++) {
+            counts[s.charAt(i) - 'a']++;
         }
-        return null;
-    }
-
-    public static void main(String[] args) {
-        char[] a = new char[]{'e', 'g', 'f', 'i', 'h', 'o', 'n', 's', 'r', 'u', 't', 'w', 'v', 'x', 'z'};
-        String[] numbers = new String[]{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        List<String> data = new ArrayList<>();
-        for (char item : a) {
-            int count = 0;
-            for (String number : numbers) {
-                if (number.contains(Character.toString(item))) {
-                    count++;
-                }
+        StringBuilder sb = new StringBuilder();
+        // step2: search by order : zero, eight
+        for (int i : priority) {
+            int k = Integer.MAX_VALUE;
+            // 比如对于 zero 而言每个数字 计算最多能成多少 组， 比如 1组 zero， 所以需要排查多少个 z， 多少个 e， 多少个 r
+            for (char c : ss[i].toCharArray()) {
+                k = Math.min(k, counts[c - 'a']);
             }
-            data.add(item + ":" + count);
+
+            for (char c : ss[i].toCharArray()) {
+                counts[c - 'a'] -= k;
+            }
+            while (k-- > 0) {
+                sb.append(i);
+            }
         }
-        data.sort(Comparator.comparing(o -> o.split(":")[1]));
-        System.out.println(JSONObject.toJSONString(data));
+        char[] cs = sb.toString().toCharArray();
+        Arrays.sort(cs);
+        return String.valueOf(cs);
     }
 }
