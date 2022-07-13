ec49d1abf65e89587af812daf5266925822e3eb4
feyfree <feyfree@163.com>
2022-07-13 18:32:46 +0800

    [REV] add new leetcode template

---
+++ src/main/java/com/feyfree/leetcode/p0445/Solution.java
@@ -0,0 +1,9 @@
+package com.feyfree.leetcode.p0445;
+
+import com.feyfree.leetcode.commons.ListNode;
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
