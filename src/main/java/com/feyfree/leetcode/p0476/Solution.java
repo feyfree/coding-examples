f49ba5cd28b50c80b9ec4625965966ddb06a08d0
Brisk <feyfree@163.com>
2022-07-28 22:16:47 +0800

    [REV] add new leetcode solution

--- src/main/java/com/feyfree/leetcode/p0476/Solution.java
+++ src/main/java/com/feyfree/leetcode/p0476/Solution.java
@@ -24,5 +24,6 @@ public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.findComplement(Integer.MAX_VALUE));
+
     }
 }
