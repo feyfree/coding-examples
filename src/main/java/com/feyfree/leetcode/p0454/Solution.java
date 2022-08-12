c80eb4b8be7f55fd25ec9668e91c71cdf4bb63ea
feyfree <feyfree@163.com>
2022-08-12 18:06:49 +0800

    [REV] add leetcode solution

--- src/main/java/com/feyfree/leetcode/p0454/Solution.java
+++ src/main/java/com/feyfree/leetcode/p0454/Solution.java
@@ -1,13 +1,33 @@
 package com.feyfree.leetcode.p0454;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * https://leetcode.cn/problems/4sum-ii/
  *
  * @author leilei
  */
 class Solution {
-    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+    private final Map<Integer, Integer> a = new HashMap<>();
 
-        return 0;
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        int result = 0;
+        calculateDualSum(nums1, nums2, a);
+        for (int k : nums3) {
+            for (int i : nums4) {
+                result += a.getOrDefault(-(k + i), 0);
+            }
+        }
+        return result;
+    }
+
+    private void calculateDualSum(int[] nums1, int[] nums2, Map<Integer, Integer> a) {
+        for (int k : nums1) {
+            for (int i : nums2) {
+                int sum = k + i;
+                a.put(sum, a.getOrDefault(sum, 0) + 1);
+            }
+        }
     }
 }
